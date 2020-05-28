113b25685528cd9570f8f1a9dd8014f30f7eafd9
harshthakur066 <harshthakur066@gmail.com>
2020-05-28 22:24:19 +0530

    server setup, connected to mongoDB and addd run script

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+require("./models/Post");
+
+mongoose.connect(
+  "mongodb+srv://myuser:intern@post-intern-hnzfp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connenting to mongo", err);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// require("./routes/postRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
