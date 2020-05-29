be81a90e36bb5221408572692af9975dc60d10d0
harshthakur066 <harshthakur066@gmail.com>
2020-05-29 10:57:18 +0530

    added state to the comment box and post request connectted

--- client/src/components/comment-box/CommentBox.jsx
+++ client/src/components/comment-box/CommentBox.jsx
@@ -1,12 +1,50 @@
-import React from "react";
-import { Form } from "semantic-ui-react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Form, Message } from "semantic-ui-react";
 
 const CommentBox = () => {
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const postComment = async (body) => {
+    try {
+      setErrorMsg("");
+      await axios.post("/api/v1/post", body);
+      alert("Comment Posted");
+    } catch (err) {
+      setErrorMsg(err.message);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      name: name,
+      boddy: comment,
+    };
+    postComment(newPost);
+  };
+
   return (
     <div>
-      <Form>
-        <Form.Input fluid label="Your Nmae" placeholder="Enter Your Name..." />
-        <Form.TextArea label="Comment" placeholder="Type a comment..." />
+      <Form onSubmit={onSubmit} error={!!errorMsg}>
+        <Form.Input
+          fluid
+          label="Your Nmae"
+          placeholder="Enter Your Name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Form.TextArea
+          label="Comment"
+          placeholder="Type a comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        {/* {success ? (
+          <h4 style={{ color: "green" }}>Your comment has been posted!!!</h4>
+        ) : null} */}
+        <Message error header="Oops!" content={errorMsg} />
         <Form.Button floated="right" negative>
           Post Comment
         </Form.Button>
